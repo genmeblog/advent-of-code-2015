1bbe9c5443881aaf2105a1145e3c1e79f56c9e32
generateme <generateme.blog@gmail.com>
2019-12-03 01:14:10 +0100

    days 1 and 2

--- src/advent_of_code_2015/day01.clj
+++ src/advent_of_code_2015/day01.clj
@@ -3,3 +3,18 @@
 
 (set! *unchecked-math* :warn-on-boxed)
 (set! *warn-on-reflection* true)
+
+(defn convert [c] (case c \( 1 \) -1 0))
+
+(def data (map convert (slurp (io/resource "day01.txt"))))
+
+(def part-1 (reduce + data))
+
+(defn find-step
+  [d]
+  (->> (reductions + 0 d)
+       (map-indexed vector)
+       (drop-while (comp not #{-1} second))
+       (ffirst)))
+
+(def part-2 (find-step data))
--- src/advent_of_code_2015/day02.clj
+++ src/advent_of_code_2015/day02.clj
@@ -5,9 +5,19 @@
 (set! *unchecked-math* :warn-on-boxed)
 (set! *warn-on-reflection* true)
 
-;; load data
-(def program (mapv read-string
-                   (-> (io/resource "day02.txt")
-                       (slurp)
-                       (s/split #","))))
+(defn parse-line [s] (sort (map read-string (s/split s #"x"))))
 
+(def dimensions (map parse-line (-> (io/resource "day02.txt")
+                                    (io/reader)
+                                    (line-seq))))
+
+(defn calc-feets
+  ^long [[^long d1 ^long d2 ^long d3]]
+  (+ (* 3 d1 d2) (* 2 d2 d3) (* 2 d3 d1)))
+
+(defn calc-ribbon
+  ^long [[^long d1 ^long d2 ^long d3]]
+  (+ d1 d1 d2 d2 (* d1 d2 d3)))
+
+(def part-1 (reduce + (map calc-feets dimensions)))
+(def part-2 (reduce + (map calc-ribbon dimensions)))
